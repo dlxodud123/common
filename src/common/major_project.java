5371ebde13016885605fac1ec529d48dffcf1b10
taeyoung <xodud5080@naver.com>
2024-02-16 15:01:10 +0900

    제발

---
+++ src/common/major_project.java
@@ -0,0 +1,22 @@
+package common;
+
+import java.util.Scanner;
+
+public class major_project {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("====전자 제품 선택====\n1. 에어컨\n2. 티비\n3. 안마기\n>>> ");
+		int func = sc.nextInt();
+		
+		switch (func) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+		}
+	}
+}
